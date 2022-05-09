f6aeff4663ef3135108ebbc7157d4cb48f964ed1
Ilka Collado <student07@Ilka-Collado.home>
2022-05-09 15:06:43 -0400

    resolving conflict

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
+import React from 'react';
+import './App.css';
 import Header from './components/Header';
 import PostContainer from './components/PostContainer';
 import AddPost from './components/AddPost';
 import Footer from './components/Footer';
-import './App.css';
-const baseURL = 'https://babble-tr-ijc.herokuapp.com/blog'
+
 function App() {
   return (
-    <div className="App">
+   <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header/>
       </header>
-
-      <PostContainer />
+      <Footer/>
     </div>
   );
 }
