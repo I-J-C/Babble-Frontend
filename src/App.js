8228d97e9006813be3e1f278b0baee97c67ae8d0
Tsani Rhodes <rhodestsani@gmail.com>
2022-05-09 14:59:31 -0400

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import PostContainer from './components/PostContainer';
-import AddPost from './components/AddPost';
+// import PostContainer from './components/PostContainer';
+// import AddPost from './components/AddPost';
 import Footer from './components/Footer';
 
 function App() {
