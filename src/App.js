8fc56cf914b8a2e70c432987fe59a2fa75e14099
Tsani Rhodes <rhodestsani@gmail.com>
2022-05-09 14:16:24 -0400

    header added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import Header from './components/header';
-import PostContainer from './components/postContainer';
-import AddPost from './components/addPost';
-import Footer from './components/footer';
+import React from 'react';
+import Header from './components/Header';
+// import PostContainer from './components/postContainer';
+// import AddPost from './components/addPost';
+// import Footer from './components/footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header/>
       </header>
     </div>
   );
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 const Post = () => {
     return (
         <div></div>
