dca0405fa3658a638dae47a86d4d3ceb7d1389da
Tsani Rhodes <rhodestsani@gmail.com>
2022-05-09 14:37:30 -0400

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 const Footer = () => {
     return (
-        <div></div>
+        <div>
+            <h4>Created by Tsani Rhodes and Jessie Collado</h4>
+            <div className="contact">
+                Contact Us
+            </div>
+            <a href="https://www.linkedin.com/in/tsanirhodes/">Connect with Tsani</a>
+            <a href="https://www.linkedin.com/in/ijessiec/"> Connect with Jessie</a>
+        </div>
     )
 }
 
